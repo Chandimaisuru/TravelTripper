e06257c36ce05a9f32bd012fecad75c1047be915
Chandimaisuru <isuruchandima92@gmail.com>
2024-01-27 03:49:49 +0530

    hero img done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,54 +1,78 @@
 import React from 'react'
-import'../styles/home.css'
-import { Container,Row, Col } from 'reactstrap'
-// import heroImg from '../assets/images/hero-img01.jpg'
-// import heroImg02 from '../assets/images/hero-img02.jpg'
-// import heroVideo from '../assets/images/hero-video.mp4'
+import '../styles/home.css'
+import { Container, Row, Col } from 'reactstrap'
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
 import worldImg from '../assets/images/world.png'
 import Subtitle from '../shared/Subtitle'
 
 function Home() {
   return (
-  <>
-   
-   <section>
+    <>
 
-     <Container>
-        
-        <Row>
-           <Col lg='6'>
-              
+      <section>
+
+        <Container>
+
+          <Row>
+            <Col lg='6'>
+
               <div className='hero__content'>
                 <div className="hero__subtitle d-flex align-item-center">
 
-                  <Subtitle subtitle={"Know Before You Go"}/>
+                  <Subtitle subtitle={"Know Before You Go"} />
 
                   <img src={worldImg} alt="" />
 
-                  
+
 
                 </div>
 
-             
+
                 <h1>Traveling opens the door to Creating{""} <span className='highlight'>memories</span></h1>
                 <p>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, sit consequuntur nesciunt odit omnis veritatis modi sint. Corrupti ipsam facere ut accusamus rerum hic, consequuntur aliquid ad voluptates incidunt quibusdam voluptate beatae harum laboriosam sint numquam dignissimos nihil aperiam tempora.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, sit consequuntur nesciunt odit omnis veritatis modi sint. Corrupti ipsam facere ut accusamus rerum hic, consequuntur aliquid ad voluptates incidunt quibusdam voluptate beatae harum laboriosam sint numquam dignissimos nihil aperiam tempora.
                 </p>
               </div>
-              
-           </Col>
 
-        </Row>
+            </Col>
+
+         <Col lg='2'>
+
+              <div className='hero__img-box'>
+                <img src={heroImg} alt="" />
+              </div>
+
+            </Col> 
+
+            <Col lg='2'>
+
+              <div className='mt-4 hero__img-box'>
+                <video src={heroVideo} alt="" controls />
+              </div>
+
+            </Col>
+
+            <Col lg='2'>
+
+              <div className='mt-5 hero__img-box'>
+                <img src={heroImg02} alt="" />
+              </div>
+
+            </Col>
+
+          </Row>
 
 
-     </Container>
+        </Container>
 
 
-   </section>
-  
-  
-  
-  </>
+      </section>
+
+
+
+    </>
   )
 }
 
---
+++ frontend/src/shared/SearchBar.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import '../shared/search-bar.css'
+
+function SearchBar() {
+  return (
+    <div>SearchBar</div>
+  )
+}
+
+export default SearchBar
