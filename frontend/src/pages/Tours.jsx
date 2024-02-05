4badcfe89fcb460147a1c479265710b092e0f9b2
Chandimaisuru <isuruchandima92@gmail.com>
2024-02-05 11:44:46 +0530

    Tour Section done

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CommonSection from '../shared/CommonSection'
 
 import '../styles/tour.css'
@@ -10,6 +10,15 @@ import { Col, Container,Row } from 'reactstrap';
 
 
 function Tours() {
+
+  const [pageCount,setPageCount] = useState(0);
+  const [page,setPage] =useState(0);
+
+  useEffect (() =>{
+    const pages = Math.ceil(5/4); //later we will use backend count
+    setPageCount(pages);
+
+  }, [page])
   return (
     <>
     
@@ -32,10 +41,12 @@ function Tours() {
              </Col>
            ))}
 
-           {/* <Col lg='12'>
+           <Col lg='12'>
                <div className='gap-3 mt-4 pagination d-flex align-items-center justify-content-center'>
                 {[...Array(pageCount).keys()].map(number =>(
-                  <span key={number} onClick={number}>
+                  <span key={number} onClick={()=> setPage(number)}
+                  className={page === number ? "active__page" : ""}
+                  >
                     {number + 1}
 
                   </span>
@@ -44,10 +55,11 @@ function Tours() {
                 }
 
                </div>
-           </Col> */}
+           </Col> 
         </Row>
        </Container>
      </section>
+     <Newsletter/>
     
     </>
   )
